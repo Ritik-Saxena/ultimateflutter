064b8715cabcdbb42a8a1174555e9a07a9bc7e55
Ritik Saxena <62079355+Ritik-Saxena@users.noreply.github.com>
2021-08-29 00:55:02 +0530

    Add files via upload

---
+++ Marvels UI/marvelui/lib/config/routes/routes.dart
@@ -0,0 +1,5 @@
+class MarvelRoutes {
+  static final spidermanPage = '/spiderman';
+  static final ironmanPage = '/ironman';
+  static final swipeCard = '/swipe';
+}
---
+++ Marvels UI/marvelui/lib/pages/pages.dart
@@ -0,0 +1,3 @@
+export 'package:marvelui/pages/spiderman_page.dart';
+export 'package:marvelui/pages/ironman_page.dart';
+export 'package:marvelui/pages/swipe.dart';
